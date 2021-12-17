959b86ac5228e12ba3e1916135606e003bec1677
Valentine Dupuis <valgrind@Valentines-MacBook-Pro.local>
2021-12-17 17:25:15 -0500

    Add function which create a csv file + km_estimation function

--- price_estimation.py
+++ price_estimation.py
@@ -1,12 +1,32 @@
-import tools
+from tools import read_data
 import sys
 
+Theta0 = 0
+Theta1 = 0
+
+def km_estimation():
+    global Theta0
+    try:
+        val = int(input("Enter a km value: "))
+    except:
+        sys.exit("Error: you must enter a digit")
+
+    km_estim = Theta0 + (Theta1 * val)
+    print(km_estim)
+    Theta0 += 1
+
 if __name__ == "__main__":
-    
-    if (sys.argv[0]):
-        filename = sys.argv[0]
+
+    if (len(sys.argv) >= 2):
+        filename = sys.argv[1]
     else:
         filename = "data.csv"
     data = read_data(filename)
-
-    print(sys.argv[0])
+    theta_file = read_data("theta_file.csv")
+    try:
+        Theta0 = float(theta_file['row1'])
+        Theta1 = float(theta_file['row2']) # TODO: Securisez le int
+    except:
+        sys.exit("Error: Failed to Read theta file must be number")
+    
+    km_estimation()
--- tools.py
+++ tools.py
@@ -6,10 +6,23 @@ def read_data(filename):
         with open(filename) as csvfile:
          reader = csv.DictReader(csvfile)
          for row in reader:
-             print(row['km'], row['price'])
+             print(row)
         return(row)
         
     except:
         sys.exit("Error: can't read input data file")
+
+def create_thetafile(header, array_of_theta):
+    with open('theta_file.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        
+        writer.writerow(header)
+        writer.writerow(array_of_theta)
+
+# header = ['row1', 'row2']
+# array_of_theta = ['0.123', '12.4']
+
+# create_thetafile(header, array_of_theta)
+# read_data("theta_file.csv")
 
 #read_data("data.csv")
