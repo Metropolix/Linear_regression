5a7e8be2c783c5a785abe26239997e8664718c20
Valentine Dupuis <valgrind@Valentines-MacBook-Pro.local>
2021-12-19 09:51:59 -0500

    add first draft of training algorithm

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Linear_regression
 This project is a good introduction to machine learning
+
+There is two file: 
+*price_estimation.py* to get an prediction 
+*training.py* to train prediction algorithm
+
+# Installation
+
+## Set up a Virtual Env 
+
+### 1- Set up a virtual env
+
+ ```
+     => python3 -m venv env 
+     => source env/bin/activate 
+```
+
+
+
+
--- traing.py
+++ traing.py
@@ -1,18 +1,35 @@
 import tools 
 
-def linear_regression(data, theta0, theta1):
+def gradient_descent(data, array_of_theta):
     i = 0
     j = 0
     learning_rate = 0.01
-    estimate_price_gap = 0
+    tmp_deriv_0 = 0
+    tmp_deriv_1 = 0
 
     for counter, car in enumerate(data):
-        print(counter)
-        estimate_price = theta0 + (theta1 * float(car['km']))
-        estimate_price_gap += estimate_price - float(car['price'])
-  
-    tmp_theta0 = learning_rate * (1/(counter+1)) * estimate_price_gap
-    tmp_theta1 = learning_rate * (1/(counter+1)) * estimate_price_gap * float(car['km'])
+            print(counter)
+            estimate_price = array_of_theta[0] + (array_of_theta[1] * float(car['km']))
+            tmp_deriv_0 += estimate_price - float(car['price'])
+            tmp_deriv_1 += (estimate_price - float(car['price'])) * float(car['km'])
+
+    array_of_theta[0] -= learning_rate * (1/(counter+1)) * tmp_deriv_0
+    array_of_theta[1] -= learning_rate * (1/(counter+1)) * tmp_deriv_1
+    print( array_of_theta[0], array_of_theta[1])
+    return (array_of_theta)
+
+def linear_regression(data, theta0, theta1):
+    number_of_iterations = 10
+
+    array_of_theta = [theta0, theta1]
+    for i in range(number_of_iterations):
+        array_of_theta = gradient_descent(data, array_of_theta)
+        print(array_of_theta[0], array_of_theta[1])
+    estimate_price = array_of_theta[0] + (array_of_theta[1] * float(data[1]['km']))
+    print( estimate_price, data[1]['price'])
+    print(array_of_theta[0], array_of_theta[1])
+
+
 
 if __name__ == "__main__":
 
