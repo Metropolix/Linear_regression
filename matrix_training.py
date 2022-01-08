fef16df8fae294e561ffe4d2c14bf22a422d6343
Valentine Dupuis <valgrind@Valentines-MacBook-Pro.local>
2022-01-08 15:57:58 -0500

    Working gradient descent with small values

--- matrix_training.py
+++ matrix_training.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy import genfromtxt
-
 import matplotlib.pyplot as plt
+from tools import read_data
 
 def show_all_datas(X, y, theta, cost):
     print("X: ", X)
@@ -12,13 +12,31 @@ def show_all_datas(X, y, theta, cost):
     print("--------")
     print("cost:", cost)
 
+def plot_data(X, y, theta, cost):
+    predictions = hypothesis(X, theta)
+    plt.scatter(X[:,1], y)
+    plt.plot(X[:,1], predictions, color='red')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+
+def load_data_matrix_from_csv(filename):
+    '''Goal is to read data from csv and set correct matrix with first row as 1'''
+    my_data = genfromtxt(filename, delimiter=',')
+    my_data = np.delete(my_data, (0), axis=0) # Remove first row (csv title row)
+    my_data = my_data[np.argsort(my_data[:, 0])] # Sort the matrix by first column (X)
+    X, y = np.split(my_data, 2, axis=1)
+    b = np.ones((y.size,1)) 
+    X = np.hstack((b,X)) # Add a first column (of 1) to X 
+    return(X, y)
+
 def hypothesis(X, theta):
     predictions = np.dot(X, theta)
     return(predictions)
 
 def gradient_descent(X, y, theta, alpha, m):
     ''' Gradient descent algorithm'''
-    predictions = np.dot(X, theta)
+    predictions = hypothesis(X,theta)
     res = sum((predictions - y) * X)
     res = res[np.newaxis]
     theta = theta - (alpha * (1/m) * res.T)
@@ -26,56 +44,23 @@ def gradient_descent(X, y, theta, alpha, m):
 
 def cost_function(X, y, theta):
     '''Goal is to minimize this function'''
-
-    predictions = np.dot(X, theta)
+    predictions = hypothesis(X, theta)
     sqrt_error = sum(np.square(predictions - y))
     cost = (1/(2*m)) * sqrt_error
-
     return(cost)
 
 if __name__ == "__main__":
- 
-    ''' Parsing'''
-    my_data = genfromtxt('test_decrement.csv', delimiter=',')
-    my_data = np.delete(my_data, (0), axis=0)
-    my_data = my_data[np.argsort(my_data[:, 0])]
-
-    X, y = np.split(my_data, 2, axis=1)
-    b = np.ones((y.size,1))
-    X = np.hstack((b,X))
-
     ''' Variable initialisation'''
+    X, y = load_data_matrix_from_csv('test_decrement.csv')
     theta = np.array([[0],[0]])
-    alpha = 0.001
+    alpha = 0.01
     m = y.size
-    num_iter = 700
+    num_iter = 3000
     cost = 0
     cost_history = np.array([])
 
-    # show_all_datas(X, y, theta, cost)
-    ''' Feature scaling'''
-
-    ''' Cost function'''
-    cost = cost_function(X, y, theta)
-    print(cost)
-
-    ''' Gradient descent'''
-
     for i in range(0, num_iter):
-        cost = cost_function(X, y, theta)
         theta = gradient_descent(X, y, theta, alpha, m)
-        
         cost_history = np.append(cost_history, cost_function(X, y, theta))
     
-    print(cost_history)
-    print(theta)
-    ''' Predictions'''
-    predictions = hypothesis(X, theta)
-    # print(predictions)
-    ''' Plot data'''
-    plt.scatter(list(range(0,num_iter)), cost_history)
-    # plt.scatter(X[:,1], y)
-    # plt.scatter(X[:,1], predictions, color='red')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
+    plot_data(X, y, theta, cost)
